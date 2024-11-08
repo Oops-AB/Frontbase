941c13142b62fb555c570e5738dcde44af11ff3a
Malte Tancred <malte@oops.se>
2024-11-08 12:44:22 +0100

    Handle multiple concurrent connections to file-based databases.

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         // 🇩🇰 A Swift module for accessing Frontbase databases.
-        .package(url: "https://github.com/Oops-AB/FrontbaseNIO.git", from: "1.5.0"),
+        .package(url: "https://github.com/Oops-AB/FrontbaseNIO.git", from: "1.6.0"),
 
         // 🗄 Core services for creating database integrations.
         .package(url: "https://github.com/vapor/sql-kit.git", from: "3.0.0"),
